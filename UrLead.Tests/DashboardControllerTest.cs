8c19e23bbdf12869a7af75d9b6be849c76cf399b
HazemLenin <63303495+HazemLenin@users.noreply.github.com>
2022-08-23 01:19:52 +0200

    added change categories buttons

---
+++ UrLead.Tests/DashboardControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace UrLead.Tests
+{
+    public class DashboardControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public DashboardControllerTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("/Index")]
+        public async Task TestEndpoints(string url)
+        {
+            // Act
+            var response = await _client.GetAsync(url);
+
+            // Assert
+            Assert.False(response.IsSuccessStatusCode);
+        }
+    }
+}
---
+++ UrLead.Tests/LeadsControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using UrLead.Data;
+
+namespace UrLead.Tests
+{
+    public class LeadsControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+        private readonly ApplicationDbContext _context;
+
+        public LeadsControllerTest(WebApplicationFactory<Program> factory, ApplicationDbContext context)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+            _context = context;
+        }
+
+        [Theory]
+        [InlineData("/Index")]
+        public async Task TestEndpoints(string url)
+        {
+            // Act
+            var response1 = await _client.GetAsync(url);
+
+            // Assert
+            Assert.False(response1.IsSuccessStatusCode);
+        }
+    }
+}
